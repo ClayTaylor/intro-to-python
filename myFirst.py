f8c60956eb72d094fafc32d88db76f451aa5f6ea
ClayTaylor <claydevelops@gmail.com>
2020-03-02 12:52:52 -0600

    Created a Summation Calculator

--- myFirst.py
+++ myFirst.py
@@ -1,22 +1,15 @@
 """
-calculator
-take in two inputs from user
-add them up
-Present the answer to the user
+-- Building a Calculator --
+take in 2 inputs from the user
+add those inputs together
+return the sum of those two inputs back to the user
 """
 
-# ask the user to input a
-a = input("Please type a number: ")
-# ask the user to input b
-b = input("Please type another number: ")
-
-# convert a and b to integers
-a = int(a)
-b = int(b)
-
-# add a + b put in answer in sum
-sum = a + b
-# report the sum to the user
-print("{} + {} = {}".format(a, b, sum))
-
-
+# First Input from the User stored in variable x & converting the input to a number.
+x = int(input("Enter the first number here: "))
+# Second Input from the User stored in variable y & converting the input to a number.
+y = int(input("Enter the second number here: "))
+# Combining those inputs in a new variable named sum
+sum = x + y
+# Printing out the variable sum to the user
+print("The sum of x and y is: " + str(sum))
